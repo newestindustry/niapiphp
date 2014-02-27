9558b8c6d65b405aecc759c9ef5c46e344ae9bc8
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2014-02-27 12:11:54 +0100

    Added a backward compatible function for session_status so we can make our claim that you need php 5.3+ instead of 5.4+

--- src/NI.php
+++ src/NI.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The NI class
- * 
+ *
  * @author Martijn van Maasakkers
  * @package NIApiPHP
  */
@@ -10,11 +10,11 @@
 /**
  * NI class.
  */
-class NI 
+class NI
 {
     /**
      * The static API class pointer
-     * 
+     *
      * @var NI\Api
      * @access public
      * @static
@@ -24,9 +24,9 @@ class NI
 
     /**
      * token
-     * 
+     *
      * (default value: false)
-     * 
+     *
      * @var bool
      * @access public
      * @static
@@ -34,17 +34,17 @@ class NI
     public static $token = false;
     /**
      * profile
-     * 
+     *
      * @var mixed
      * @access private
      */
     private $profile;
-    
+
     /**
      * namespace
-     * 
+     *
      * (default value: "niapi")
-     * 
+     *
      * @var string
      * @access public
      */
@@ -52,7 +52,7 @@ class NI
 
     /**
      * __construct function.
-     * 
+     *
      * @access public
      * @param array $config (default: array())
      * @return void
@@ -60,31 +60,31 @@ class NI
     public function __construct($config = array())
     {
         // Make sure to start a session
-        if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
-    
+        if ($this->checkSessionStatus() === FALSE) {session_start();}
+
         self::setApi(new \NI\Api($this));
         $this->readApiConfig($config);
-        
+
         if(isset($_SESSION[self::$namespace]['token']) && $_SESSION[self::$namespace]['token']->access_token) {
             $this->setToken($_SESSION[self::$namespace]['token']->access_token);
         }
     }
-    
+
     /**
      * readApiConfig function.
-     * 
+     *
      * @access public
      * @param array $config (default: array())
      * @return void
      */
     public function readApiConfig($config = array())
     {
-        $this->getApi()->readConfig($config);        
+        $this->getApi()->readConfig($config);
     }
-    
+
     /**
      * login function.
-     * 
+     *
      * @access public
      * @param bool $register (default: false)
      * @return void
@@ -102,15 +102,15 @@ class NI
                 throw new \NI\Oauth\Exception($_GET['error_description']);
             } elseif(isset($_GET['code'])) {
                 self::$token = $this->getApi()->getToken();
-            }    
+            }
         }
-        
+
         return self::$token;
     }
-    
+
     /**
      * logout function.
-     * 
+     *
      * @access public
      * @return void
      */
@@ -118,10 +118,10 @@ class NI
     {
         $this->getApi()->logout();
     }
-    
+
     /**
      * getNamespace function.
-     * 
+     *
      * @access public
      * @return string
      */
@@ -129,10 +129,10 @@ class NI
     {
         return self::$namespace;
     }
-    
+
     /**
      * setToken function.
-     * 
+     *
      * @access public
      * @param string $token
      * @return void
@@ -143,8 +143,29 @@ class NI
     }
 
     /**
+     * checkSessionStatus function.
+     *
+     * This is a backward compatible function for session_status
+     * so we can make our claim that you need php 5.3+ instead
+     * of 5.4+
+     *
+     * @access public
+     * @return boolean
+     */
+    public function checkSessionStatus() {
+        if ( php_sapi_name() !== 'cli' ) {
+            if ( version_compare(phpversion(), '5.4.0', '>=') ) {
+                return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+            } else {
+                return session_id() === '' ? FALSE : TRUE;
+            }
+        }
+        return FALSE;
+    }
+
+    /**
      * getApi function.
-     * 
+     *
      * @access public
      * @static
      * @return \NI\Api
@@ -153,10 +174,10 @@ class NI
     {
         return self::$api;
     }
-    
+
     /**
      * setApi function.
-     * 
+     *
      * @access public
      * @static
      * @param \NI\Api $api
@@ -165,5 +186,7 @@ class NI
     public static function setApi($api)
     {
         self::$api = $api;
-    }    
+    }
+
+
 }
